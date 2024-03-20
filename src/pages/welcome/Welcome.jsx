d34eeadd8c98cbb5cd30a292e2caf117a7be15cb
pitersizovdev <pitersizov@gmail.com>
2024-03-20 23:06:04 +0300

    0.1.1

--- src/pages/welcome/Welcome.jsx
+++ src/pages/welcome/Welcome.jsx
@@ -1,19 +1,28 @@
 import React from 'react'
 import styles from './welcome.module.scss'
-import Swipe from '../../components/swipe/Swipe'
 import Avatar from '../../assets/img/avatar.png'
 
 const Welcome = () => {
   return (
     <div className={styles.container}>
+      <div className={styles.logoContainer}>
+        <div className={styles.piter}>PITER</div>
+        <div className={styles.sizov}>SIZOV</div>
+      </div>
 
       <div className={styles.chattingContainer}>
-        <div className={styles.message}><p>Привет! Меня зовут Петр,<br/> я frontend разработчик.</p></div>
+        <div className={styles.messageContainer}>
+          <div className={styles.message}><span>Привет!</span></div>
+          <div className={styles.message}><span>Меня зовут Петр –<br/>я frontend разработчик.</span></div>
+          <div className={styles.message}><span>Проведи вправо, чтобы<br/>посмотреть мои работы.</span></div>
+        </div>
         <div className={styles.avatar}><img src={Avatar}/></div>
       </div>
 
-      <div className={styles.swiperContainer}>
-        <div className={styles.swipeToggle}>0</div>
+      <div className={styles.swipeContainer}>
+        <a>Резюме PDF</a>
+        <div className={styles.swiperContainer}></div>
+        <div className={styles.swiperToggle}>0</div>
         <div className={styles.swiper}>Посмотреть</div>
       </div>
     </div>
